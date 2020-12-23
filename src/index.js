5eac2368082057c4a41610c506fddccfaef0c46f
Umair Arshad <Umair6622@gmail.com>
2020-12-24 04:41:59 +0500

    Fix the required changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import home from './home';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  home();
-  return element;
-}
-
-document.body.appendChild(component());
+home();
